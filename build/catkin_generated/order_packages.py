46ab25a0e43bf67a0feb481ec482a04b2e2ef02c
Blebot0 <keshav29kapur@gmail.com>
2020-06-19 17:20:06 +0530

    ARTR

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/blebot/ar_tag/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/blebot/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/blebot/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ build/gazebo_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/blebot/ar_tag/src/gazebo_bot
+# Build directory: /home/blebot/ar_tag/build/gazebo_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
