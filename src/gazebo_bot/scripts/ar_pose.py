dbfc92aba01ff2cfb044a3cb71e85c6b7ca367a6
Aditi Rao <57457307+aditirao7@users.noreply.github.com>
2020-06-21 23:06:33 +0530

    Update ar_pose.py

--- src/gazebo_bot/scripts/ar_pose.py
+++ src/gazebo_bot/scripts/ar_pose.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from ar_track_alvar_msgs.msg import AlvarMarker
+from ar_track_alvar_msgs.msg import AlvarMarkers
 from geometry_msgs.msg import Point, Twist
 
 def callback(msg):
@@ -21,7 +21,7 @@ def listener():
 
     rospy.init_node('ar_pose')
     print(1)
-    rospy.Subscriber("/ar_pose_marker", AlvarMarker, callback)
+    rospy.Subscriber("/ar_pose_marker", AlvarMarkers, callback)
     print(2)
     rospy.spin()
 
