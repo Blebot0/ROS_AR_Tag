50153a7202c4cf23897124a0bde0010a51a5c6f5
Blebot0 <keshav29kapur@gmail.com>
2020-06-27 19:46:09 +0530

    ARTR

--- src/gazebo_bot/scripts/ar_pose.py
+++ src/gazebo_bot/scripts/ar_pose.py
@@ -4,6 +4,9 @@ from std_msgs.msg import String
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from geometry_msgs.msg import Point, Twist
 import numpy as np
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import math
+
 
 def callback(msg):
     j = 0   
@@ -19,24 +22,33 @@ def callback(msg):
 
     for i in range(j):
         print("ID:       " + str(msg.markers[i].id))
-        pose_x = msg.markers[i].pose.pose.position.x 
-        pose_y = msg.markers[i].pose.pose.position.y
-        pose_z = msg.markers[i].pose.pose.position.z 
+        pos_x = msg.markers[i].pose.pose.position.x *100
+        pos_y = msg.markers[i].pose.pose.position.y *100
+        pos_z = msg.markers[i].pose.pose.position.z *100
         
         or_x = msg.markers[i].pose.pose.orientation.x
         or_y = msg.markers[i].pose.pose.orientation.y
         or_z = msg.markers[i].pose.pose.orientation.z 
         or_w = msg.markers[i].pose.pose.orientation.w
-        print("x = {0}, y = {1}, z = {2}".format(pose_x, pose_y, pose_z))
-        print("or_z = {0}, or_y = {1}, or_z = {2}, or_w = {3}".format(or_x, or_y, or_z, or_w)) 
-    ## ADD WHATEVER YOU WANT HERE
 
-
+        quaternion = (or_x, or_y, or_z, or_w)
+        euler = euler_from_quaternion(quaternion)
+        
+        
+        roll = math.degrees(euler[0])+180
+        pitch = math.degrees(euler[1])+180
+        yaw = math.degrees(euler[2])+180
+      
+        dist = pow( pow(pos_x,2) + pow(pos_y, 2) + pow(pos_z, 2) , 2) 
+        print("x = {0}cm, y = {1}cm, z = {2}cm".format(pos_x  , pos_y , pos_z  ))
 
 def listener():
-
+    global pub
     rospy.init_node('ar_pose')
     rospy.Subscriber("/ar_pose_marker", AlvarMarkers, callback)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+    twist = Twist()
+    
     rospy.spin()
 
 if __name__ == '__main__':
---
+++ src/gazebo_bot/scripts/ar_pose_single.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import rospy
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from geometry_msgs.msg import Point, Twist
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import math
+
+
+pos_x = 0
+pos_y = 0
+pos_z = 0
+
+def callback(msg):
+    global pos_x, pos_y, pos_z
+    if len(msg.markers)==1:
+        print("ID:       " + str(msg.markers[0].id))
+        pos_x = msg.markers[0].pose.pose.position.x *100
+        pos_y = msg.markers[0].pose.pose.position.y *100
+        pos_z = msg.markers[0].pose.pose.position.z *100
+
+        print("x = {0}cm, y = {1}cm, z = {2}cm".format(pos_x  , pos_y , pos_z  ))
+    else:
+        speed.linear.x = 0
+        speed.angular.z = 0
+        pub.publish(speed)
+
+
+rospy.init_node('ar_pose')
+
+rospy.Subscriber("/ar_pose_marker", AlvarMarkers, callback)
+pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+
+r = rospy.Rate(10)
+speed = Twist()
+
+while not rospy.is_shutdown():
+
+        speed.linear.x = -pos_z/500
+        speed.angular.z = -pos_x/100
+        if pos_z < 30:
+            speed.linear.x = 0
+            speed.angular.z = 0
+        print(speed)
+    
+        pub.publish(speed)
+    
+        r.sleep()
+
+
+    
+
